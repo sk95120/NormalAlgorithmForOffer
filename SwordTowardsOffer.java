8fdf18669fc323b1269460d3a3254606c465b51a
sk95120 <765871277@qq.com>
2019-03-24 17:29:08 +0800

    提交信息。

--- SwordTowardsOffer.java
+++ SwordTowardsOffer.java
@@ -120,3 +120,57 @@ public int[] multipy(int[] A){
         return false;
     }
 
+#54 查看是否为数字
+public boolean isNumeric(char[] str) {
+	if (str == null)
+		return false;
+	int index = 0;
+	int ecount = 0;
+	int point = 0;
+// 如果第一个字符是符号就跳过
+	if (str[0] == '-' || str[0] == '+')
+		index++;
+	for (int i = index; i < str.length; i++) {
+		if (str[i] == '-' || str[i] == '+') {
+			if (str[i-1] != 'e' && str[i-1] != 'E')
+				return false;
+			continue;
+		}
+		if (str[i] == 'e' || str[i] == 'E') {
+			ecount++;
+			if (ecount > 1)
+			return false;
+			if (i == 0 || str[i-1] < 48 || str[i-1] > 57 || i == str.length-1)
+				return false;
+			point++;
+			continue;
+		}
+		if (str[i] == '.') {
+			point++;
+		if (point > 1)
+			return false;
+		continue;
+	}
+// 出现非数字且不是e/E则返回false（小数点和符号用continue跳过了）
+	if ((str[i] < 48 || str[i] > 57) && (str[i] != 'e') && (str[i] != 'E'))
+		return false;
+	}
+	return true;
+}
+
+# 55 请实现一个函数用来找出字符流中第一个只出现一次的字符
+char[] chars = new char[256];
+StringBuilder sb = new StringBuilder();
+public void Insert(char ch) {
+    sb.append(ch);
+    chars[ch]++;
+}
+public char FirstAppearingOnce() {
+    char[] str = sb.toString().toCharArray();
+    for (char c : str) {
+        if (chars[c] == 1) {
+            return c;
+        }
+    }
+    return '#';
+}
